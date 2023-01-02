d3c51e1afcdd85264e55f50fe32d95e1ffc534f1
usama-mahmood <darindahp@gmail.com>
2023-01-02 17:47:51 +0500

    allowed all hosts

--- StorefrontBackend2/settings/dev.py
+++ StorefrontBackend2/settings/dev.py
@@ -9,7 +9,8 @@ DEBUG = os.getenv("DEBUG")
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # DATABASES = {
